89da1dbefc00d80c695b07df0914420514f050b5
Tony Li <tonyli@cs.stonybrook.edu>
2018-01-28 19:42:15 -0500

    improved MissionControl

--- qualbot/MissionControl.java
+++ qualbot/MissionControl.java
@@ -3,32 +3,45 @@ import java.util.PriorityQueue;
 import java.util.ArrayList;
 import java.util.Iterator;
 public class MissionControl{
-	MarsScanner scanner;
-	PriorityQueue<ArrayList<MapLocation>> areas;
-	ArrayList<String> used;
-	public MissionControl(GameController gc){
-		scanner = new MarsScanner(gc.startingMap(Planet.Mars),Planet.Mars);
-		scanner.loadVirtualMap();
-		areas = scanner.scan();
-		used = new ArrayList<String>();
-	}
+    MarsScanner scanner;
+    ArrayList<ArrayList<MapLocation>> areas;
+    ArrayList<String> used;
+    int i = 0;
+    public MissionControl(GameController gc){
+	scanner = new MarsScanner(gc.startingMap(Planet.Mars),Planet.Mars);
+	scanner.loadVirtualMap();
+	areas = new ArrayList<ArrayList<MapLocation>>(scanner.scan());
+	used = new ArrayList<String>();
+    }
 
-	public MapLocation getLocation(){
-		int i = (int)(Math.random()*areas.size()/4);
-		Iterator<ArrayList<MapLocation>> it = areas.iterator();
-		ArrayList<MapLocation> pls = null;
-		int count = 0;
-		while(it.hasNext()){
-			pls = it.next();
-			if(i==count)
-				break;
-			count++;
-		}
-		MapLocation loc = scanner.landRandom(pls);
-		//System.out.println(loc);
-		if(used.contains(loc.toJson()))
-			return null;
-		used.add(loc.toJson());
-		return loc;
+    public MapLocation getLocation(){
+	
+	if (i > areas.size()/3){
+	    i = 0;
 	}
+	
+	ArrayList<MapLocation> area = areas.get(i);
+	MapLocation target = scanner.landRandom(area);
+	area.remove(target);
+	i++;
+	return target;
+
+	/*
+	Iterator<ArrayList<MapLocation>> it = areas.iterator();
+	ArrayList<MapLocation> pls = null;
+	int count = 0;
+	while(it.hasNext()){
+	    pls = it.next();
+	    if(i==count)
+		break;
+	    count++;
+	}
+	MapLocation loc = scanner.landRandom(pls);
+	//System.out.println(loc);
+	if(used.contains(loc.toJson()))
+	    return null;
+	used.add(loc.toJson());
+	return loc;
+	*/
+    }
 }
